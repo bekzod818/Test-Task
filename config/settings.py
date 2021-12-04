247d4f1352c40e3bf0dc97b9194ab86f37613a97
bekzod818 <bekzod030900@gmail.com>
2021-12-04 15:59:49 +0500

    set

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'template'
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
