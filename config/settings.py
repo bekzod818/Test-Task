2745907e445e3c6536233f03ae12ef85e52093d4
bekzod818 <bekzod030900@gmail.com>
2022-01-17 00:46:54 +0500

    crispy

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Tashqi_apps
+    "crispy_forms",
+    "crispy_bootstrap5",
     # My_apps
     'employee.apps.EmployeeConfig'
 ]
@@ -131,3 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
